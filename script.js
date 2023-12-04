d272d294abe26deba0e22a68480d2b5c34effd32
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-12-04 22:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
+//complete this code
+class Animal {
+	constructor(species){
+		this._species = species;
+	}
+	get species(){
+		return this._species; 
+	}
+	makeSound(){
+	console.log("The" + this.species + "makes a sound");
+	}
+}
 
-class Square extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof");
+	}
+}
+
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
